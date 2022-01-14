72cb193ebf3db6756fb879215b7ab50e193716ff
Martin-Bruel <martin.bruel999@gmail.com>
2022-01-14 16:23:07 +0100

    initial back commit

---
+++ back/index.js
@@ -0,0 +1,25 @@
+const express = require('express');
+const {WebSocketServer} = require('ws')
+
+const portWS = 3000
+const wss = new WebSocketServer({ port: portWS });
+
+var sockets = [];
+
+console.log('Listening WEB SOCKET on port '+portWS+'!')
+
+wss.on('connection', function connection(ws) {
+    console.log("new connection")
+    sockets.push(ws);
+});
+
+
+const app = express();
+const portREST = 8080
+const message = "tada"
+
+app.post('/', (req, res) => {
+    res.status(200).json('ok');
+    sockets.forEach((s) => s.send(message));
+});
+app.listen(portREST, () => console.log('Listening REST API on port '+portREST+'!'))
