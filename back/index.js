d5386b565c8326628b1a3dfbba6da2907cb0b2c5
Martin-Bruel <martin.bruel999@gmail.com>
2022-01-20 22:00:45 +0100

    fix backend to accept json query

--- back/index.js
+++ back/index.js
@@ -1,6 +1,9 @@
 const express = require('express');
 const {WebSocketServer} = require('ws')
 
+CORS_ORIGIN_ALLOW_ALL = true
+
+
 const portWS = 3000
 const wss = new WebSocketServer({ port: portWS });
 
@@ -18,8 +21,19 @@ const app = express();
 const portREST = 8080
 const message = "tada"
 
+app.use(function(req, res, next) {
+    res.header("Access-Control-Allow-Origin", "*");
+    res.setHeader('Access-Control-Allow-Methods', 'GET, POST, OPTIONS, PUT, PATCH, DELETE');
+    res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
+    next();
+});
+
+app.use(express.json());
+
+
 app.post('/', (req, res) => {
+    console.log("message receive : "+req.body.message);
     res.status(200).json('ok');
-    sockets.forEach((s) => s.send(message));
+    sockets.forEach((s) => s.send(req.body.message));
 });
 app.listen(portREST, () => console.log('Listening REST API on port '+portREST+'!'))
