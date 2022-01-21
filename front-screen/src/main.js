c7a4026378bb08534be6e7094639be8c59977f62
tbenoist1 <timothee.benoist@gmail.com>
2022-01-21 14:32:43 +0100

    refactor

--- front-screen/src/main.js
+++ front-screen/src/main.js
@@ -1,17 +1,8 @@
-// The Vue build version to load with the `import` command
-// (runtime-only or standalone) has been set in webpack.base.conf with an alias.
 import Vue from 'vue'
-import App from './App'
-import router from './router'
-import VueNativeSock from 'vue-native-websocket'
+import App from './App.vue'
 
-// Vue.use(VueNativeSock, 'ws://localhost:9090')
 Vue.config.productionTip = false
 
-/* eslint-disable no-new */
 new Vue({
-  el: '#app',
-  router,
-  components: { App },
-  template: '<App/>'
-})
+  render: h => h(App),
+}).$mount('#app')
