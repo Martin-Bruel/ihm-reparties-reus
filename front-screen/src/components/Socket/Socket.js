34ab3cd332df5db0d399e6c81c0fb50e5b9d91d2
Xabi Merlo <xabi.merlo@etu.univ-cotedazur.fr>
2022-01-21 14:49:58 +0100

    Try to add multiTouch to front-screen

---
+++ front-screen/src/components/Socket/Socket.js
@@ -0,0 +1,18 @@
+import Card from "../Card";
+
+export default {
+    data: () => ({ time: null }),
+    
+    function(){
+      let connection = new WebSocket('ws://192.168.54.136:3000/');
+      connection.onmessage = (event) => {
+        // Vue data binding means you don't need any extra work to
+        // update your UI. Just set the `time` and Vue will automatically
+        // update the `<h2>`.
+        this.time = event.data;
+      }
+    },
+    components: {
+      Card
+    },
+}
