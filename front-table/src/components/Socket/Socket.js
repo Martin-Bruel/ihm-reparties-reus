a332da1c16b8eadd657d4328e86dbdd7c332e89e
Vincent-Delmotte <vincent.delmotte@etu.unice.fr>
2022-01-21 16:26:46 +0100

    multiple screen

--- front-table/src/components/Socket/Socket.js
+++ front-table/src/components/Socket/Socket.js
@@ -1,11 +1,36 @@
-import axios from 'axios'
+
 
 export default {
-    data: () => ({ time: null }),
+    data: () => ({ time: null, message: null }),
     methods: {
-      async submit(){
-        await axios.post("http://192.168.54.136:8080",{message: "SECOND POTEAU PALLOOOOOOOIIIIIS"},{"Access-Control-Allow-Origin": "*"});
-        console.log("COUCOU");
+      submit(id){
+        const json = {
+          id: id,
+          message: this.message,
+        };
+        
+        this.connection.send(JSON.stringify(json));
+      },
+
+      sendMessage: function(message) {
+        console.log(this.connection);
+        this.connection.send(message);
       }
+    },
+
+    created: function() {
+      const PORT = "3000";
+      const ID = "0";
+      const IP = "192.168.210.136";
+  
+      this.connection = new WebSocket(`ws://${IP}:${PORT}?id=${ID}`);
+      this.connection.onopen = () => {
+        
+      };
+      this.connection.onmessage = () => {
+        // Vue data binding means you don't need any extra work to
+        // update your UI. Just set the `time` and Vue will automatically
+        // update the `<h2>`.
+      };
     }
 }
