cc1ce576e3665ba682c3e6967bbfb5b737b7ba7f
Martin-Bruel <martin.bruel999@gmail.com>
2022-02-03 16:20:50 +0100

    pushing websocket

--- back/index.js
+++ back/index.js
@@ -1,39 +1,37 @@
-const express = require('express');
-const {WebSocketServer} = require('ws')
-
-CORS_ORIGIN_ALLOW_ALL = true
+const { WebSocketServer } = require('ws');
 
 
-const portWS = 3000
-const wss = new WebSocketServer({ port: portWS });
+const portWS = 3000;
+const wss = new WebSocketServer({ port: portWS }); 
 
-var sockets = [];
+var screens = {};
+var table;
 
 console.log('Listening WEB SOCKET on port '+portWS+'!')
 
-wss.on('connection', function connection(ws) {
-    console.log("new connection")
-    sockets.push(ws);
+wss.on('connection', function connection(ws, req) {
+    const data = new URLSearchParams(req.url.replace('/',''));
+    if(data.get('id') == 0){
+
+        console.log('Table is connected');
+        table = ws;
+        ws.on('message', onTableMessage);
+    }
+    else{
+        console.log(`Screen ${data.get('id')} is connected`);
+        screens[data.get('id')] = ws;
+        ws.on('message', onScreenMessage);
+    }
 });
 
+function onScreenMessage(data){
+    console.log('message recieve : ' + data);
+    const json = JSON.parse(data);
+    table.send(json.message);
+}
 
-const app = express();
-const portREST = 8080
-const message = "tada"
-
-app.use(function(req, res, next) {
-    res.header("Access-Control-Allow-Origin", "*");
-    res.setHeader('Access-Control-Allow-Methods', 'GET, POST, OPTIONS, PUT, PATCH, DELETE');
-    res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
-    next();
-});
-
-app.use(express.json());
-
-
-app.post('/', (req, res) => {
-    console.log("message receive : "+req.body.message);
-    res.status(200).json('ok');
-    sockets.forEach((s) => s.send(req.body.message));
-});
-app.listen(portREST, () => console.log('Listening REST API on port '+portREST+'!'))
+function onTableMessage(data){
+    console.log('message recieve : ' + data);
+    const json = JSON.parse(data);
+    screens[json.id].send(json.message)
+}
