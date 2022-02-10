e821ebfa1c23faa36a52eb9034b56f37038ad9c9
Charles Godaert <charles.godaert@etu.univ-amu.fr>
2022-02-10 19:22:14 +0100

    Dark mode

--- front-screen/src/App.vue
+++ front-screen/src/App.vue
@@ -16,12 +16,19 @@ export default {
 </script>
 
 <style>
+html, body, #app {
+  width: 100%;
+  height: 100%;
+  margin: 0;
+  padding: 0;
+  background-color: #181a1b;
+}
 #app {
   font-family: Avenir, Helvetica, Arial, sans-serif;
   -webkit-font-smoothing: antialiased;
   -moz-osx-font-smoothing: grayscale;
   text-align: center;
-  color: #2c3e50;
+  color: whitesmoke;
   margin-top: 60px;
 }
 
--- front-table/src/App.vue
+++ front-table/src/App.vue
@@ -1,6 +1,5 @@
 <template>
   <div id="app">
-    <img alt="Vue logo" src="./assets/logo.png">
     <Socket/>
     <hr/><br>
     <div style="display: flex;justify-content: center;">
@@ -84,13 +83,20 @@ export default {
 </script>
 
 <style>
+
+html, body, #app {
+  width: 100%;
+  height: 100%;
+  margin: 0;
+  padding: 0;
+  background-color: #181a1b;
+}
 #app {
   font-family: Avenir, Helvetica, Arial, sans-serif;
   -webkit-font-smoothing: antialiased;
   -moz-osx-font-smoothing: grayscale;
   text-align: center;
   color: #2c3e50;
-  margin-top: 60px;
 }
 .list-card {
     display: inline-block;
